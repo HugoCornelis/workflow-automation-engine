f5ea8abd99cf72b6ddf5d5b7d53bdcf95e4286b3
Hugo Cornelis <hugo.cornelis@gmail.com>
2019-06-15 20:58:16 +0200

    Added git support for the model-container

--- perl/Neurospaces/Developer.pm
+++ perl/Neurospaces/Developer.pm
@@ -306,6 +306,9 @@ sub default_packages_read()
 					  'neurospaces',
 					 ],
 				 version_control => {
+						     git => {
+							     remote => 'github',
+							    },
 						     branch_name => 0,
 						     port_number => 4693,
 						    },
