d7413399c1bd4b101be0d4cc21db7e99309c0f03
Hugo Cornelis <hugo.cornelis@gmail.com>
2025-05-19 08:22:16 +0200

    Add a second python target to feature-testing-workflow.
    This target triggers the problem of double importing of Python
    functions.

--- tests/specifications/workflow-configurations/feature-testing/feature-testing-commands-more-python.py
+++ tests/specifications/workflow-configurations/feature-testing/feature-testing-commands-more-python.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 
-def pure_python_python_command(argv):
-    command = "echo 'python_command from bash: execute_command() (1)'"
+def more_python_python_command(argv):
+    command = "echo 'python_command from bash (1)'"
     execute_command(command)
     command_array = [
-        "echo 'python_command from bash: execute_command_array() (2)'",
-        "echo 'python_command from bash: execute_command_array() (3)'"
+        "echo 'python_command from bash (2)'",
+        "echo 'python_command from bash (3)'"
     ]
     execute_command_array(command_array)
 
 
-def pure_python_python_command_completions(command, argv):
+def more_python_python_command_completions(command, argv):
     completions_hash = {
            "1._<your_completion_without_spaces_here>": "explain_the_purpose_of_the_completion_here",
            "2._<another_completion_here>": "the_numbers_preserve_the_order_for_this_example",
            "3._Don't_use_spaces_in_your_completions,_because_they_confuse_bash": "further_explanation",
-           "4._Look_at_the_completion_function_of_pure-python_single_command_completions_to_understand_how_it_works": "further_explanation",
+           "4._Look_at_the_completion_function_of_more-python_single_command_completions_to_understand_how_it_works": "further_explanation",
           }
 
     return completions_hash
 
 
-def pure_python_python_command_help(command, perl_argv):
+def more_python_python_command_help(command, perl_argv):
     return f"python_python_command {command} help page"
 
